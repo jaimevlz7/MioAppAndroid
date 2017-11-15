2db4b65d07b7f9738fbc4b7df8cb5632b34b7a4e
Japo <japolacios@gmail.com>
2017-11-14 20:20:49 -0500

    First Aproach to map layout and implementation

--- app/src/main/java/com/mio/app/mioapp/MainActivity.java
+++ app/src/main/java/com/mio/app/mioapp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.mio.app.mioapp;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    long startTime;
+    ImageView logo_splash;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        startTime = System.currentTimeMillis();
+
+        logo_splash =(ImageView) findViewById(R.id.splash_logo_img);
+
+        /*
+        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                R.animator.splash_object_intro);
+        set.setTarget(logo_splash);
+        set.start();*/
+
+        Animation splash_intro = AnimationUtils.loadAnimation(this, R.anim.splash_object_intro);
+        logo_splash.startAnimation(splash_intro);
+
+        goToLiveView();
+    }
+
+    public void goToLiveView(){
+        int timer = 1000;
+        while (System.currentTimeMillis()-startTime<timer) {
+
+            if (System.currentTimeMillis() - startTime >= timer) {
+                Intent intent = new Intent(this, live_view.class);
+                startActivity(intent);
+
+            }
+        }
     }
 }
